195eed198e12a2094afdf549d9406d31ece979b3
kirill <kshypachov@outlook.com>
2025-09-22 09:28:24 +0300

    Start write homeassistant support

--- src/mqtt/mqtt_helpers.c
+++ src/mqtt/mqtt_helpers.c
@@ -3,73 +3,166 @@
 //
 
 #include "mqtt_helpers.h"
+#include "../global_var.h"
+#include <errno.h>
+#include <arpa/inet.h>
 
-// static void client_init(struct mqtt_client *client)
-// {
-// 	mqtt_client_init(client);
-//
-// 	broker_init();
-//
-// 	/* MQTT client configuration */
-// 	client->broker = &broker;
-// 	client->evt_cb = mqtt_evt_handler;
-// 	client->client_id.utf8 = (uint8_t *)MQTT_CLIENTID;
-// 	client->client_id.size = strlen(MQTT_CLIENTID);
-// 	client->password = NULL;
-// 	client->user_name = NULL;
-// #if defined(CONFIG_MQTT_VERSION_5_0)
-// 	client->protocol_version = MQTT_VERSION_5_0;
-// #else
-// 	client->protocol_version = MQTT_VERSION_3_1_1;
-// #endif
-//
-// 	/* MQTT buffers configuration */
-// 	client->rx_buf = rx_buffer;
-// 	client->rx_buf_size = sizeof(rx_buffer);
-// 	client->tx_buf = tx_buffer;
-// 	client->tx_buf_size = sizeof(tx_buffer);
-//
-// 	/* MQTT transport configuration */
-// #if defined(CONFIG_MQTT_LIB_TLS)
-// #if defined(CONFIG_MQTT_LIB_WEBSOCKET)
-// 	client->transport.type = MQTT_TRANSPORT_SECURE_WEBSOCKET;
-// #else
-// 	client->transport.type = MQTT_TRANSPORT_SECURE;
-// #endif
-//
-// 	struct mqtt_sec_config *tls_config = &client->transport.tls.config;
-//
-// 	tls_config->peer_verify = TLS_PEER_VERIFY_REQUIRED;
-// 	tls_config->cipher_list = NULL;
-// 	tls_config->sec_tag_list = m_sec_tags;
-// 	tls_config->sec_tag_count = ARRAY_SIZE(m_sec_tags);
-// #if defined(MBEDTLS_X509_CRT_PARSE_C) || defined(CONFIG_NET_SOCKETS_OFFLOAD)
-// 	tls_config->hostname = TLS_SNI_HOSTNAME;
-// #else
-// 	tls_config->hostname = NULL;
-// #endif
-//
-// #else
-// #if defined(CONFIG_MQTT_LIB_WEBSOCKET)
-// 	client->transport.type = MQTT_TRANSPORT_NON_SECURE_WEBSOCKET;
-// #else
-// 	client->transport.type = MQTT_TRANSPORT_NON_SECURE;
-// #endif
-// #endif
-//
-// #if defined(CONFIG_MQTT_LIB_WEBSOCKET)
-// 	client->transport.websocket.config.host = SERVER_ADDR;
-// 	client->transport.websocket.config.url = "/mqtt";
-// 	client->transport.websocket.config.tmp_buf = temp_ws_rx_buf;
-// 	client->transport.websocket.config.tmp_buf_len =
-// 						sizeof(temp_ws_rx_buf);
-// 	client->transport.websocket.timeout = 5 * MSEC_PER_SEC;
-// #endif
-//
-// #if defined(CONFIG_SOCKS)
-// 	mqtt_client_set_proxy(client, &socks5_proxy,
-// 			      socks5_proxy.sa_family == AF_INET ?
-// 			      sizeof(struct sockaddr_in) :
-// 			      sizeof(struct sockaddr_in6));
-// #endif
-// }
+#define ha_universal_config_topik_template	 "%s/%s/%s_%s/%s%u/config"
+
+#define dev_system						"cedar"
+#define dev_common_name					"CedarSwitch"
+#define dev_manufacturer_name			"Manufacturer"
+#define dev_model_name					"CedarSwitch-3R3I-POWER"
+
+#define component_sensor				"sensor"
+#define component_binary_sensor			"binary_sensor"
+#define component_switch				"switch"
+
+#define component_input					"input"
+#define component_input_name			"Вхід"
+
+#define dev_class_switch				component_switch
+#define dev_class_switch_name			"Перемикач"
+
+#define component_battery				"battery"
+#define component_battery_name  		"Батарея"
+
+#define component_power_supply			"power_supply"
+#define component_power_supply_name 	"Джерело живлення"
+
+#define dev_class_energy				"energy"
+#define dev_class_energy_name   	    "Енергія"
+#define dev_class_energy_state			"\"state_class\": \"total_increasing\",\n"
+#define dev_class_energy_unit_of_measurement	"kWh"
+
+#define dev_class_voltage				"voltage"
+#define dev_class_voltage_name			"Напруга"
+#define dev_class_voltage_unit_of_measurement	"V"
+
+#define dev_class_power					"power"
+#define dev_class_power_name			"Активна потужність"
+#define dev_class_power_unit_of_measurement	"W"
+
+#define dev_class_apparent_power		"apparent_power"
+#define dev_class_apparent_power_name	"Повна потужність"
+#define dev_class_apparent_power_unit_of_measurement	"VA"
+
+#define dev_class_power_factor			"power_factor"
+#define dev_class_power_factor_name	    "Коефіцієнт потужності"
+#define dev_class_power_factor_unit_of_measurement "%"
+
+#define dev_class_current				"current"
+#define dev_class_current_name			"Струм"
+#define dev_class_current_unit_of_measurement	"A"
+
+
+
+static char mqtt_dev_identifier[MQTT_UNIC_IDENTIFIER_LENGTH] = {0};
+static char	dev_conf_ip [INET6_ADDRSTRLEN]	= {0}; //IPv4 or IPv6
+char home_assistant_prefix[] = {"homeassistant"};
+
+
+
+int set_device_id(const uint8_t* id, unsigned const int id_len){
+
+    if (id == NULL || id_len == 0) {
+        return -EINVAL;
+    }
+
+    /* Нужно 2 символа на байт + 1 под '\0' */
+    const size_t need = id_len * 2u + 1u;
+    if (need > sizeof(mqtt_dev_identifier)) {
+        return -ENOSPC;
+    }
+
+    // Проходим по каждому байту идентификатора
+    for (unsigned int i = 0; i < id_len; i++) {
+        // Конвертируем каждый байт в два символа и добавляем в строку
+        sprintf(&mqtt_dev_identifier[i * 2], "%02X", id[i]);
+    }
+    return 0;
+}
+
+const char *get_device_id(void)
+/* Возвращает указатель на постоянную строку с ID (или NULL, если не установлен). */
+{
+    /* k_mutex_lock(&dev_id_lock, K_FOREVER); */
+    bool empty = (mqtt_dev_identifier[0] == '\0');
+    /* k_mutex_unlock(&dev_id_lock); */
+    return empty ? NULL : mqtt_dev_identifier;
+}
+
+// валидируем IPv4/IPv6 и только потом сохраняем */
+int set_device_conf_ip(const char *ip_str)
+{
+    if (ip_str == NULL) {
+        return -EINVAL;
+    }
+
+    /* Проверим, валиден ли IPv4 или IPv6 */
+    struct in_addr  v4;
+    struct in6_addr v6;
+
+    int ok_v4 = inet_pton(AF_INET,  ip_str, &v4) == 1;
+    int ok_v6 = inet_pton(AF_INET6, ip_str, &v6) == 1;
+
+    if (!ok_v4 && !ok_v6) {
+        return -EINVAL; /* не распознали адрес */
+    }
+
+    const int n = snprintf(dev_conf_ip, sizeof(dev_conf_ip), "%s", ip_str);
+    if (n < 0) {
+        return n;
+    }
+    if (n >= (int)sizeof(dev_conf_ip)) {
+        return -ENOSPC;
+    }
+    return 0;
+}
+
+const char *get_device_conf_ip(void)
+{
+    return dev_conf_ip[0] ? dev_conf_ip : NULL;
+}
+
+int get_config_topik_string (char * buff, uint32_t buff_len, mqtt_ha_dev_type_t dev_type, uint8_t obj_number){
+
+	strcpy(buff, "");
+	switch (dev_type) {//%s/%s/%s_%s/%s%u/config
+		case INPUT_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_binary_sensor, dev_system, mqtt_dev_identifier, component_input, obj_number);
+			break;
+		case OUTPUT_DEVICE:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_switch, dev_system, mqtt_dev_identifier, dev_class_switch, obj_number);
+			break;
+	    case VOLTAGE_DIAGNOSTIC_BATT_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, component_battery, obj_number);
+			break;
+	    case VOLTAGE_DIAGNOSTIC_POW_SUPL_SENSOR:
+	    	snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, component_power_supply, obj_number);
+	    	break;
+		case ENERGY_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, dev_class_energy, obj_number);
+			break;
+		case VOLTAGE_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, dev_class_voltage, obj_number);
+			break;
+		case POWER_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, dev_class_power, obj_number);
+			break;
+		case APPARENT_POWER_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, dev_class_apparent_power, obj_number);
+			break;
+		case POWER_FACTOR_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, dev_class_power_factor, obj_number);
+			break;
+		case CURRENT_SENSOR:
+			snprintf(buff, buff_len, ha_universal_config_topik_template, home_assistant_prefix, component_sensor, dev_system, mqtt_dev_identifier, dev_class_current, obj_number);
+			break;
+
+		default:
+			return -1;
+			break;
+	}
+	return 0;
+}
--- src/mqtt/mqtt_helpers.h
+++ src/mqtt/mqtt_helpers.h
@@ -7,5 +7,22 @@
 
 #include <zephyr/net/mqtt.h>
 
+typedef enum{
+    INPUT_SENSOR			= 1,
+    OUTPUT_DEVICE			= 2,
+    ENERGY_SENSOR			= 3,
+    POWER_SENSOR			= 4,
+    VOLTAGE_SENSOR			= 5,
+    POWER_FACTOR_SENSOR		= 6,
+    CURRENT_SENSOR 			= 7,
+    APPARENT_POWER_SENSOR	= 8,
+    VOLTAGE_DIAGNOSTIC_BATT_SENSOR	= 9,
+    VOLTAGE_DIAGNOSTIC_POW_SUPL_SENSOR	= 10,
+}mqtt_ha_dev_type_t;
+
+int set_device_id(const uint8_t* id, unsigned const int id_len);
+const char *get_device_id(void);
+int set_device_conf_ip(const char *ip_str);
+const char *get_device_conf_ip(void);
 
 #endif //CEDAR_SWITCH_3IN3OUT_POWER_MQTT_HELPERS_H
