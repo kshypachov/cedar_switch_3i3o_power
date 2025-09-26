50a974eeca361b667f6675686757de53280548e5
kshypachov <128974084+kshypachov@users.noreply.github.com>
2025-09-26 23:21:02 +0300

    Board revision 2. Changed MCU from stm32f411ce to stm32f412ce

---
+++ boards/cedar_switch_3in3out_power_rev2/board.cmake
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(jlink "--device=STM32F412CE" "--speed=4000")
+
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+#board_set_flasher(jlink)
+#board_set_debugger(jlink)
+#board_set_flasher(stm32cubeprogrammer)
+#board_set_debugger(stm32cubeprogrammer)
+
+#board_set_flasher_ifnset(jlink)
+# board_set_debugger_ifnset(jlink)
