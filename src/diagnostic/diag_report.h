4b1cee3e417dd4fe4b9138eaa03193cf42566f1d
kshypachov <128974084+kshypachov@users.noreply.github.com>
2025-10-20 23:10:53 +0300

    Function for network diagnostic

---
+++ src/diagnostic/diag_report.h
@@ -0,0 +1,10 @@
+//
+// Created by Kirill Shypachov on 28.09.2025.
+//
+
+#ifndef CEDAR_SWITCH_3IN3OUT_POWER_DIAG_REPORT_H
+#define CEDAR_SWITCH_3IN3OUT_POWER_DIAG_REPORT_H
+
+void net_diag_start_periodic(k_timeout_t interval);
+
+#endif //CEDAR_SWITCH_3IN3OUT_POWER_DIAG_REPORT_H
