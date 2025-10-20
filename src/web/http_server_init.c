0022662d1f5e8900e5b12aaead3968bc80afd766
kshypachov <128974084+kshypachov@users.noreply.github.com>
2025-10-20 23:20:04 +0300

    Add capture headers, User-Agent, Authorization, Cookie for support authorization. Add init authentication system

--- src/web/http_server_init.c
+++ src/web/http_server_init.c
@@ -5,17 +5,22 @@
 #include <zephyr/net/http/service.h>
 #include <zephyr/net/http/server.h>
 #include <zephyr/logging/log.h>
+#include "authentication.h"
 
 static uint16_t http_static_service_port = 80;
 static uint16_t http_api_service_port = 8080;
 
 LOG_MODULE_REGISTER(http_server_init);
 
+HTTP_SERVER_REGISTER_HEADER_CAPTURE(capture_user_agent, "User-Agent");
+HTTP_SERVER_REGISTER_HEADER_CAPTURE(capture_authorization, "Authorization");
+HTTP_SERVER_REGISTER_HEADER_CAPTURE(capture_cookie, "Cookie");
 
 HTTP_SERVICE_DEFINE(http_static_service, "0.0.0.0", &http_static_service_port, 2, 10, NULL, NULL);
 
 HTTP_SERVICE_DEFINE(http_api_service, "0.0.0.0", &http_api_service_port, 1, 10, NULL, NULL);
 
 void app_http_server_init(void) {
+    authentication_init();
     http_server_start();
 }
